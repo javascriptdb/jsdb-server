e2987d57b59ab85b875715945291d3cae102653e
Diego Franco <diegofhrg@gmail.com>
2022-08-06 10:28:02 -0600

    feat[2-auth-google]: Add rules for users

--- .jsdb/db/users/rules.js
+++ .jsdb/db/users/rules.js
@@ -1,1 +1,7 @@
-export default () => false
+export default function ({req}){
+  const apiKey = req.get('X-API-Key');
+  if(apiKey !== process.env.API_KEY) {
+    throw new Error('Invalid API Key');
+  }
+  return true;
+}
